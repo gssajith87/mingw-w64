ef70ff030c81165e82098a5489af710e3536b847
NightStrike <NightStrike@4407c894-4637-0410-b4f5-ada5f102cad1>
2007-08-22 20:10:08 +0000

    Updated for svn access to headers and crt
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@43 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildsystem/makebuildroot.sh
+++ experimental/buildsystem/makebuildroot.sh
@@ -4,7 +4,7 @@ TGT=x86_64-pc-mingw32
 
 [ -h mingw ] || ln -s $TGT mingw
 
-for i in $TGT build build/binutils build/binutils/build64 build/gcc-svn build/gcc-svn/build64 build/kai; do
+for i in $TGT build build/binutils build/binutils/build64 build/gcc-svn build/gcc-svn/build64 build/mingw build/mingw/build64; do
   [ -d $i ] && echo $i already exists || mkdir $i
 done
 
@@ -16,15 +16,9 @@ cvs -Qz 9 -d :pserver:anoncvs@sourceware.org:/cvs/src co binutils
 echo "Downloading gcc.." && cd ../gcc-svn
 svn -q checkout svn://gcc.gnu.org/svn/gcc/trunk gcc
 
-echo "Downloading crt.." && cd ../kai
-wget -Nq http://downloads.sourceforge.net/mingw/mingw-w64-crt-20070802.tar.bz2
-echo "Downloading headers.."
-wget -Nq http://downloads.sourceforge.net/mingw/mingw-w64-headers-20070802.tar.bz2
-echo "Extracting.."
-for i in *bz2; do
-  tar xf $i
-done
-[ -d $PF/$TGT/include ] && echo $PF/$TGT/include already exists || mv include $PF/$TGT
+echo "Downloading crt and headers.." && cd ../mingw
+svn -q co https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64/trunk .
+[ -d $PF/$TGT/include ] && echo $PF/$TGT/include already exists || mv mingw-w64-headers/include $PF/$TGT
 
 echo "Root setup complete."
 
@@ -37,14 +31,14 @@ echo "Compiling binutils.." && cd ../binutils/build64
 make install > /dev/null
 
 echo "Compiling bootstrap gcc.." && cd ../../gcc-svn/build64
-../gcc/configure --prefix=$PF --with-sysroot=$PF --target=$TGT --disable-nls --enable-languages=c $* > /dev/null && make all-gcc > /dev/null
+../gcc/configure --prefix=$PF --with-sysroot=$PF --target=$TGT --disable-nls $* > /dev/null && make all-gcc > /dev/null
 make install-gcc > /dev/null
 
-echo "Compiling crt.." && cd ../../kai/mwcrt_noat
+echo "Compiling crt.." && cd ../../mingw/mingw-w64-crt
 make prefix=$PF > /dev/null
-cp CRT_fp10.o CRT_fp8.o binmode.o txtmode.o crtbegin.o crtend.o crt1.o crt2.o dllcrt1.o dllcrt2.o /tmp/myroot/$TGT/lib
+cp -pv CRT_fp10.o CRT_fp8.o binmode.o txtmode.o crtbegin.o crtend.o crt1.o crt2.o dllcrt1.o dllcrt2.o /tmp/myroot/$TGT/lib
 for i in `find . -name "*.a"`; do
-  cp -v $i /tmp/myroot/$TGT/lib
+  cp -pv $i /tmp/myroot/$TGT/lib
 done
 
 echo "Compiling full gcc.. " && cd ../../gcc-svn/build64
