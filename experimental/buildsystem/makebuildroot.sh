3c0cfa7b97abe67c6680fa2f8d1d20950a99885e
nightstrike <nightstrike@4407c894-4637-0410-b4f5-ada5f102cad1>
2007-10-10 18:51:25 +0000

    Added makedist and noupdate support
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@109 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildsystem/makebuildroot.sh
+++ experimental/buildsystem/makebuildroot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 PF=`pwd`/root
 BD=`pwd`/build
 TGT=x86_64-pc-mingw32
@@ -6,6 +6,8 @@ DIRS="$PF $PF/$TGT $BD $BD/binutils $BD/binutils/build64 $BD/gcc-svn $BD/gcc-svn
 EXE=
 build="false"
 usecvs="true"
+update="true"
+makedist="false"
 gmpver=gmp-4.2.2
 gmp=ftp://ftp.gnu.org/gnu/gmp/${gmpver}.tar.bz2
 mpfrver=mpfr-2.3.0
@@ -33,6 +35,9 @@ while opt=$1 && shift; do
 		downloads will not be as up to date as the cvs repository, and as such this is provided
 		only as a fallback.
 
+    --noupdate  Inhibit downloading or updating the source code repositories.  This can greatly speed up
+                rebuilds when you want to get by the 'svn up' of gcc quickly.
+
     --build	Continue past the downloading stage and begin building.  First it builds binutils,
 		and then a bootstrap compiler.  Both binutils and gcc use the same basic options,
 		with gcc requiring an additional argument to specify the desired languages.  Third 
@@ -48,6 +53,8 @@ while opt=$1 && shift; do
 		Keep in mind that this is all very temporary, and is provided as a convenience.  Please
 		direct all inquiries to the sourceforge project at:
 		https://sourceforge.net/projects/mingw-w64/
+
+    --makedist  This will tar up the root directory for purposes of distribution.
     
 EOF
     exit
@@ -65,67 +72,80 @@ EOF
       usecvs="false"
       ;;
 
+    "--noupdate" )
+      update="false"
+      ;;
+
+    "--makedist" )
+      makedist="true"
+      ;;
+
     "--help" )
   esac
 done
 
-echo "Creating directory tree:"
-echo "$PF -- sysroot directory tree"
-echo "$BD -- build directory tree"
-echo ""
+if [[ $update == "true" ]]; then
+  echo "Creating directory tree:"
+  echo "$PF -- sysroot directory tree"
+  echo "$BD -- build directory tree"
+  echo ""
 
-for i in $DIRS; do
-  [ -d $i ] && echo "Warning: $i preexisting, skipping directory creation" || mkdir $i
-done
-[ -h mingw ] || ln -s $TGT $PF/mingw
+  for i in $DIRS; do
+    [ -d $i ] && echo "Warning: $i preexisting, skipping directory creation" || mkdir $i
+  done
+  [ -h mingw ] || ln -s $TGT $PF/mingw
 
+  echo "Downloading binutils.." && cd $BD/binutils
+  if [[ $usecvs == "true" ]]; then
+    cvs -Qz 9 -d :pserver:anoncvs@sourceware.org:/cvs/src co binutils
+  else
+    wget -qO- ftp://sourceware.org/pub/binutils/snapshots/binutils.tar.bz2 | tar xjf - && mv binutils-* src
+  fi
 
-echo "Downloading binutils.." && cd $BD/binutils
-if [[ $usecvs == "true" ]]; then
-  cvs -Qz 9 -d :pserver:anoncvs@sourceware.org:/cvs/src co binutils
-else
-  wget -qO- ftp://sourceware.org/pub/binutils/snapshots/binutils.tar.bz2 | tar xjf - && mv binutils-* src
+  echo "Downloading gcc.." && cd $BD/gcc-svn
+  svn -q checkout svn://gcc.gnu.org/svn/gcc/trunk gcc
+
+  echo "Downloading additional required libraries for gcc.." && cd gcc
+  for i in gmp mpfr; do
+    ver=${i}ver
+    [ -d $i ] && echo "Warning: $i preexisting, skipping download" || ( wget -qO- ${!i} | tar xjf - && mv ${!ver} $i )
+  done
+  cd ..
+
+  echo "Downloading crt and headers.." && cd $BD/mingw
+  svn -q co https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64/trunk .
+  [ -d $PF/$TGT/include ] && echo $PF/$TGT/include already exists || mv mingw-w64-headers/include $PF/$TGT
+
+  echo "Root setup complete."
 fi
 
-echo "Downloading gcc.." && cd $BD/gcc-svn
-svn -q checkout svn://gcc.gnu.org/svn/gcc/trunk gcc
+if [[ $build == "true" ]]; then
+  echo "Compiling binutils.." && cd $BD/binutils/build64
+  ../src/configure --prefix=$PF --with-sysroot=$PF --disable-nls --target=$TGT > /dev/null && make > /dev/null || exit
+  make install > /dev/null || exit
 
-echo "Downloading additional required libraries for gcc.." && cd gcc
-for i in gmp mpfr; do
-  ver=${i}ver
-  [ -d $i ] && echo "Warning: $i preexisting, skipping download" || ( wget -qO- ${!i} | tar xjf - && mv ${!ver} $i )
-done
-cd ..
+  echo "Compiling bootstrap gcc.." && cd $BD/gcc-svn/build64
+  ../gcc/configure --prefix=$PF --with-sysroot=$PF --target=$TGT --disable-nls > /dev/null && make all-gcc > /dev/null || exit
+  make install-gcc > /dev/null || exit
 
-echo "Downloading crt and headers.." && cd $BD/mingw
-svn -q co https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64/trunk .
-[ -d $PF/$TGT/include ] && echo $PF/$TGT/include already exists || mv mingw-w64-headers/include $PF/$TGT
+  echo "Compiling crt.." && cd $BD/mingw/mingw-w64-crt
+  make prefix=$PF $EXE > /dev/null || exit
+  echo "Installing crt.."
+  cp -pv CRT_fp10.o CRT_fp8.o binmode.o txtmode.o crtbegin.o crtend.o crt1.o crt2.o dllcrt1.o dllcrt2.o $PF/$TGT/lib || exit
+  echo "Installing libs.."
+  for i in `find . -name "*.a"`; do 
+    cp -p $i $PF/$TGT/lib || exit
+  done
 
-echo "Root setup complete."
+  echo "Compiling full gcc.." && cd $BD/gcc-svn/build64
+  make > /dev/null || exit
+  make install > /dev/null || exit
 
+  cd $PF
+  echo "Done building."
+fi
 
-[[ $build == "false" ]] && exit
-
-echo "Compiling binutils.." && cd $BD/binutils/build64
-../src/configure --prefix=$PF --with-sysroot=$PF --disable-nls --target=$TGT > /dev/null && make > /dev/null || exit
-make install > /dev/null || exit
-
-echo "Compiling bootstrap gcc.." && cd $BD/gcc-svn/build64
-../gcc/configure --prefix=$PF --with-sysroot=$PF --target=$TGT --disable-nls > /dev/null && make all-gcc > /dev/null || exit
-make install-gcc > /dev/null || exit
-
-echo "Compiling crt.." && cd $BD/mingw/mingw-w64-crt
-make prefix=$PF $EXE > /dev/null || exit
-echo "Installing crt.."
-cp -pv CRT_fp10.o CRT_fp8.o binmode.o txtmode.o crtbegin.o crtend.o crt1.o crt2.o dllcrt1.o dllcrt2.o $PF/$TGT/lib || exit
-echo "Installing libs.."
-for i in `find . -name "*.a"`; do 
-  cp -p $i $PF/$TGT/lib || exit
-done
-
-echo "Compiling full gcc.." && cd $BD/gcc-svn/build64
-make > /dev/null || exit
-make install > /dev/null || exit
-
-cd $PF
-echo "Done."
+if [[ $makedist == "true" ]]; then
+  cd $PF/..
+  tar cjf mingw-w64-bin-`uname`-`$PF/bin/$TGT-gcc --version | head -1 | awk '{print $4}'`.tar.bz2 --owner root --group root root
+fi
