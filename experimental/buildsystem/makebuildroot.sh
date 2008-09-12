9f5953a62eb0c311f0d0e37e3e0cd500585f8665
nightstrike <nightstrike@4407c894-4637-0410-b4f5-ada5f102cad1>
2008-09-12 22:30:43 +0000

    Update to mpfr 2.3.2
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@467 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildsystem/makebuildroot.sh
+++ experimental/buildsystem/makebuildroot.sh
@@ -19,7 +19,7 @@ out=/dev/null
 #Third part packages
 gmpver=gmp-4.2.2
 gmp=ftp://ftp.gnu.org/gnu/gmp/${gmpver}.tar.bz2
-mpfrver=mpfr-2.3.1
+mpfrver=mpfr-2.3.2
 mpfr=http://www.mpfr.org/mpfr-current/${mpfrver}.tar.bz2
 
 while opt=$1 && shift; do
