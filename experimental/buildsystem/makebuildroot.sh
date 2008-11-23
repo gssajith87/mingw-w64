41d5fdeae65942bfe58954d74a0e9c708d9f2b58
nightstrike <nightstrike@4407c894-4637-0410-b4f5-ada5f102cad1>
2008-11-23 08:20:44 +0000

    Update default gmp to 4.2.4
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@504 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildsystem/makebuildroot.sh
+++ experimental/buildsystem/makebuildroot.sh
@@ -17,7 +17,7 @@ gccrev="HEAD"
 out=/dev/null
 
 #Third part packages
-gmpver=gmp-4.2.2
+gmpver=gmp-4.2.4
 gmp=ftp://ftp.gnu.org/gnu/gmp/${gmpver}.tar.bz2
 mpfrver=mpfr-2.3.2
 mpfr=http://www.mpfr.org/mpfr-current/${mpfrver}.tar.bz2
