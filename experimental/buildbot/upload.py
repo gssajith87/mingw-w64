8eabc70717a1c07ff218fd61058ccd7e4ee7b9d4
mookmoz <mookmoz@4407c894-4637-0410-b4f5-ada5f102cad1>
2009-05-30 06:30:24 +0000

    make it possible to drive upload.py from the command line
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@876 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildbot/upload.py
+++ experimental/buildbot/upload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from ConfigParser import RawConfigParser as ConfigParser
-import getopt, os, re, stat, subprocess, sys, datetime
+import optparse, os, re, stat, subprocess, sys, datetime, getpass
 import cookielib, urllib, urllib2
 
 ### random constants
@@ -13,36 +13,105 @@ def main(argv):
   config = ConfigParser()
   config.read("config.ini")
 
-  # check that the file exists
-  (optparse, args) = getopt.gnu_getopt(argv, 'd:o:nv', ['datestamp=', 'os=', 'dry-run', 'verbose'])
+  ### get arguments
+  optparser = optparse.OptionParser(
+    usage="%prog [options] <srcfile> <destfile>",
+    description="Uploads a file to SourceForge and publishes it for mingw-w64")
+  optparser.enable_interspersed_args()
+
+  optparser.add_option("-d", "--datestamp", action="store", dest="datestamp",
+                       help="date stamp to add to the file name")
+  optparser.add_option("-n", "--dry-run", action="store_true", dest="dry_run")
+  optparser.add_option("-o", "--os", action="store", dest="os")
+  optparser.add_option("-v", "--verbose", action="count", dest="verbose")
+
+  optparser.set_default("datestamp", None)
+  optparser.set_default("os", "w64")
+  optparser.set_default("dry_run", False)
+  optparser.set_default("verbose", 0)
+
+  def configOptionCallback(option, opt_str, value, parser, *args):
+    """ an option parser callback to put things into the ini parser """
+    try:
+      section = value[:value.index(".")]
+      value = value[len(section) + 1:]
+      key = value[:value.index("=")]
+      value = value[len(key) + 1:]
+    except ValueError,e:
+      raise optparse.OptionValueError(
+        "invalid configuration value:\n  %s\nformat is <section>.<key>=<value>" % (value))
+    if not args[0].has_section(section):
+      args[0].add_section(section)
+    args[0].set(section, key, value)
+
+  optparser.add_option("-c", "--config", action="callback", type="string",
+                       help="set a configuration value section.key=value",
+                       callback=configOptionCallback, callback_args=(config,))
+
+  def configOptionReplacement(option, opt_str, value, parser, args, **kwargs):
+    """ an option parser for a shorthand for a configOptionCallback """
+    value = "%s.%s=%s" % (kwargs["section"], kwargs["key"], value)
+    configOptionCallback(option, "--config", value, parser, args)
+  optparser.add_option("-u", "--user", action="callback", type="string",
+                       help="specify sourceforge user name",
+                       callback=configOptionReplacement, callback_args=(config,),
+                       callback_kwargs={"section": "sourceforge", "key": "user"})
+  optparser.add_option("-p", "--password", action="store_true",
+                       help="prompt for sourceforge login password")
+  optparser.add_option("-k", "--key", action="callback", type="string",
+                       help="specify sourceforge ssh key",
+                       callback=configOptionReplacement, callback_args=(config,),
+                       callback_kwargs={"section": "sourceforge", "key": "sshkey"})
+  optparser.add_option("-g", "--group-id", action="callback", type="string",
+                       help="specify sourceforge group id",
+                       callback=configOptionReplacement, callback_args=(config,),
+                       callback_kwargs={"section": "sourceforge", "key": "group_id"})
+  optparser.add_option("--package-id", "--pkg-id", action="store", type="string",
+                       help="specify sourceforge package id", dest="package_id")
+  optparser.add_option("-r", "--release-id", action="store", type="string",
+                       help="specify sourceforge release id", dest="release_id")
+
+  (opts, args) = optparser.parse_args(args=argv)
   while "" == args[-1]:
     # remove any trailing empty parameters
     args.pop()
-  assert len(args) == 3
+  if (len(args) != 3):
+    optparser.print_help()
+    sys.exit(1)
   srcfile = args[1]
   destfile = args[2]
   filesize = os.stat(srcfile)[stat.ST_SIZE]
 
-  opts = {
-    "datestamp": None,
-    "os": "w64",
-    "dry-run": False,
-    "verbose": False
-  }
-  for o,a in optparse:
-    if o in ("-d", "--datestamp"):
-      assert re.match("""^_*\d{8}$""", a), \
-             "datestamp must be of form yyyymmdd (with optional leading underscore)"
-      opts["datestamp"] = "_" + a.strip("_")
-    elif o in ("-n", "--dry-run"):
-      opts["dry-run"] = True
-    elif o in ("-o", "--os"):
-      opts["os"] = a
-    elif o in ("-v", "--verbose"):
-      opts["verbose"] = True
+  ### deal with arguments that depended on other arguments
+  if opts.password:
+    # prompt for the password
+    delattr(opts, "password")
+    prompt = "Password for %s@sourceforge: " % (config.get("sourceforge", "user"))
+    password = getpass.getpass(prompt)
+    if not config.has_section("sourceforge"):
+      config.add_section("sourceforge")
+    config.set("sourceforge", "password", password)
+
+  if opts.package_id:
+    # specify the package id, based on the given os
+    config.set("sourceforge", "package-%s" % (opts.os), opts.package_id)
+    delattr(opts, "package_id")
+
+  if opts.release_id:
+    # specify the release id, based on the given os
+    config.set("sourceforge", "release-%s" % (opts.os), opts.release_id)
+    delattr(opts, "release_id")
+
+  ### check for sane arguments
+  if opts.datestamp is not None:
+    opts.datestamp = "_" + opts.datestamp.strip("_")
+    assert re.match("""^_*\d{8}$""", opts.datestamp), \
+      "datestamp must be of form yyyymmdd (with optional leading underscore)"
+
   assert filesize > 1048576, "%s is only %s bytes" %(srcfile, filesize)
   assert (-1 < srcfile.find(".tar.")) or (-1 < srcfile.find(".zip")), "%s is not a tarball" % (srcfile)
 
+  ### argument processing complete, go do things
   upload(srcfile, destfile, config, opts)
   page = publish(destfile, config, opts)
   cleanup(destfile, config, opts, page)
@@ -53,7 +122,7 @@ def upload(srcfile, destfile, config, opts):
   print " ".join(command)
   command[3] = command[3] % (config.get("sourceforge", "sshkey"), "ssh_known_hosts")
   command[5] = command[5] % (config.get("sourceforge", "user"))
-  if opts["dry-run"]:
+  if opts.dry_run:
     print "(rsync skipped due to dry-run)"
   else:
     for attempt in range(0, 5):  # 5 tries
@@ -75,7 +144,7 @@ def publish(filename, config, opts):
                 "ssl_status": 1,
                 "form_securemode": "yes",
                 "login": "Log in"}
-  if opts["verbose"]:
+  if opts.verbose > 0:
     for item in login_data:
       if item == "form_pw":
         print "%s = <*>" % (item)
@@ -89,15 +158,15 @@ def publish(filename, config, opts):
 
   # read the page and guess if we succeeded
   for line in login_page:
-    if opts["verbose"]:
+    if opts.verbose > 2:
       print line[:-1]
     assert (-1 == line.find(LOGIN_FAIL)), "sourceforge login error"
 
   ### publish files
-  if not opts["dry-run"]:
+  if not opts.dry_run:
     publish_data = {"group_id" : config.get("sourceforge", "group_id"),
-                    "package_id": config.get("sourceforge", "package-" + opts["os"]),
-                    "release_id": config.get("sourceforge", "release-" + opts["os"]),
+                    "package_id": config.get("sourceforge", "package-" + opts.os),
+                    "release_id": config.get("sourceforge", "release-" + opts.os),
                     "step2": 1,
                     "file_list[]": filename,
                     "submit": "Add Files and/or Refresh View"}
@@ -105,8 +174,8 @@ def publish(filename, config, opts):
     print "%s published to sourceforge; editing metadata..." % (filename)
   else:
     publish_data = {"group_id" : config.get("sourceforge", "group_id"),
-                    "package_id": config.get("sourceforge", "package-" + opts["os"]),
-                    "release_id": config.get("sourceforge", "release-" + opts["os"])}
+                    "package_id": config.get("sourceforge", "package-" + opts.os),
+                    "release_id": config.get("sourceforge", "release-" + opts.os)}
     publish_page = urllib2.urlopen(EDIT_URL, urllib.urlencode(publish_data))
     print "Skipping publish of %s due to dry-run..." % (filename)
   return publish_page
@@ -123,6 +192,8 @@ def cleanup(filename, config, opts, page_data):
   in_form = False
   in_select = None
   for line in page_data.readlines():
+    if opts.verbose > 2:
+      print line[:-1]
     if line.find('<form action="/project/admin/editreleases.php" method="post">') != -1:
       # new file
       item_data = {}
@@ -152,11 +223,11 @@ def cleanup(filename, config, opts, page_data):
   if re.search(r'x86.64', filename):
     processor = 'AMD64'
   extension = filename[filename.rindex("."):]
-  if opts["datestamp"] is not None and filename.find(opts["datestamp"]) != -1:
-    prefix = filename[:filename.find(opts["datestamp"])]
-    cutoff = datetime.date(int(opts["datestamp"][1:5]),
-                           int(opts["datestamp"][5:7]),
-                           int(opts["datestamp"][7:9]))
+  if opts.datestamp is not None and filename.find(opts.datestamp) != -1:
+    prefix = filename[:filename.find(opts.datestamp)]
+    cutoff = datetime.date(int(opts.datestamp[1:5]),
+                           int(opts.datestamp[5:7]),
+                           int(opts.datestamp[7:9]))
     cutoff -= datetime.timedelta(7)
   else:
     prefix = None
@@ -164,15 +235,15 @@ def cleanup(filename, config, opts, page_data):
   for item in items:
     if item["file_name"] == filename:
       edit_data = {"group_id" : config.get("sourceforge", "group_id"),
-                   "package_id": config.get("sourceforge", "package-" + opts["os"]),
-                   "release_id": config.get("sourceforge", "release-" + opts["os"]),
-                   "new_release_id": config.get("sourceforge", "release-" + opts["os"]),
+                   "package_id": config.get("sourceforge", "package-" + opts.os),
+                   "release_id": config.get("sourceforge", "release-" + opts.os),
+                   "new_release_id": config.get("sourceforge", "release-" + opts.os),
                    "file_id": item["file_id"],
                    "step3": 1,
                    "processor_id": item["processor_id"][processor],
                    "type_id": item["type_id"][filename[filename.rfind("."):]],
                    "submit": "Update/Refresh"}
-      if opts["dry-run"]:
+      if opts.dry_run:
         print "editing item %s (processor %s) skipped due to dry-run" % (
                 filename, processor)
       else:
@@ -185,12 +256,12 @@ def cleanup(filename, config, opts, page_data):
         datestamp = datetime.date(int(datestring[0:4]), int(datestring[4:6]), int(datestring[6:8]))
         if datestamp < cutoff:
           edit_data = {"group_id" : config.get("sourceforge", "group_id"),
-                       "package_id": config.get("sourceforge", "package-" + opts["os"]),
-                       "release_id": config.get("sourceforge", "release-" + opts["os"]),
+                       "package_id": config.get("sourceforge", "package-" + opts.os),
+                       "release_id": config.get("sourceforge", "release-" + opts.os),
                        "file_id": item["file_id"],
                        "im_sure": 1,
                        "step3": "Delete File"}
-          if opts["dry-run"]:
+          if opts.dry_run:
             print "deleting item %s skipped due to dry-run" % (item["file_name"])
           else:
             urllib2.urlopen(EDIT_URL, urllib.urlencode(edit_data))
