ac5e94344ec2cc13bbe880b9c9ea89e1ede4081c
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2009-06-03 13:48:52 +0000

    Begin to scan pdb files for streams.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@889 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/tools/libmsdebug/dbg_interface_pdb.c
+++ experimental/tools/libmsdebug/dbg_interface_pdb.c
@@ -7,6 +7,17 @@
 #include "dbg_interface_pdb.h"
 #include "dbg_pdb.h"
 
+/* Prototypes.  */
+static sPdbRoot2 *pdb2Root (sPdbHeader2 *pph, uint32_t *pStreams, uint32_t *pStreamBytes, int *pfUnused);
+static sPdbRoot7 *pdb7Root (sPdbHeader7 *pph, uint32_t *pStreams, uint32_t *pStreamBytes, int *pfUnused);
+static void *pdb2FileRead (sPdbHeader2 *pph, uint32_t streamBytes, void *pages, int *pfUnused);
+static void *pdb7FileRead (sPdbHeader7 *pph, uint32_t streamBytes, void *pages, int *pfUnused);
+static uint32_t pdb2FilePages (sPdbHeader2 *pph, uint32_t streamBytes, uint32_t *pPageBytes, int *pfUnused);
+static uint32_t pdb7FilePages (sPdbHeader7 *pph, uint32_t streamBytes, uint32_t *pPageBytes, int *pfUnused);
+static int pdb2FileUnused (sPdbHeader2 *pph, uint32_t streamBytes, uint32_t *pPageBytes);
+static int pdb7FileUnused (sPdbHeader7 *pph, uint32_t streamBytes, uint32_t *pPageBytes);
+
+/* Interface functions.  */
 static int pdb2_probe(const sDbgMemFile *pDFile);
 static int pdb2_load (sDbgInterface *pDCtx);
 static int pdb2_update (sDbgInterface *pDCtx);
@@ -63,9 +74,16 @@ static int pdb2_probe(const sDbgMemFile *pDFile)
 
 static int pdb2_load (sDbgInterface *pDCtx)
 {
-  if (!pDCtx)
-    return -1;
-  
+  uint32_t streams, streamBytes;
+  int fUnused = 0;
+  sPdbHeader2 *hdr = (sPdbHeader2 *) pDCtx->memfile->data;
+  sPdbRoot2 *root = pdb2Root (hdr, &streams, &streamBytes, &fUnused);
+  if (root)
+    {
+      fprintf (stderr, " # of streams: %u\n", streams);
+      free (root);
+    }
+ 
   return 0;
 }
 
@@ -81,7 +99,8 @@ static int pdb2_release (sDbgInterface *pDCtx)
 
 static sDbgMemFile *pdb2_dump (sDbgInterface *pDCtx)
 {
-  return unknown_dump (pDCtx);
+  /* return unknown_dump (pDCtx); */
+  return 0;
 }
 
 static sSymbolInterface *pdb2_search(struct sDbgInterface *pDCtx, sSymbolSearchInterface *match)
@@ -110,6 +129,15 @@ static int pdb7_probe(const sDbgMemFile *pDFile)
 
 static int pdb7_load (sDbgInterface *pDCtx)
 {
+  uint32_t streams, streamBytes;
+  int fUnused = 0;
+  sPdbHeader7 *hdr = (sPdbHeader7 *) pDCtx->memfile->data;
+  sPdbRoot7 *root = pdb7Root (hdr, &streams, &streamBytes, &fUnused);
+  if (root)
+    {
+      fprintf (stderr, " # of streams: %u\n", root->nstreams);
+      free (root);
+    }
   return unknown_load (pDCtx);
 }
 
@@ -125,7 +153,8 @@ static int pdb7_release (sDbgInterface *pDCtx)
 
 static sDbgMemFile *pdb7_dump (sDbgInterface *pDCtx)
 {
-  return unknown_dump (pDCtx);
+  /* return unknown_dump (pDCtx); */
+  return 0;
 }
 
 static sSymbolInterface *pdb7_search(struct sDbgInterface *pDCtx, sSymbolSearchInterface *match)
@@ -136,4 +165,158 @@ static sSymbolInterface *pdb7_search(struct sDbgInterface *pDCtx, sSymbolSearchI
 static sDbgMemFile *pdb7_dump_symbol(struct sDbgInterface *pDCtx, sSymbolInterface *pSym)
 {
   return unknown_dump_symbol (pDCtx, pSym);
+}
+
+/* Static helper functions.  */
+
+static sPdbRoot2 *pdb2Root (sPdbHeader2 *pph, uint32_t *pStreams, uint32_t *pStreamBytes, int *pfUnused)
+{
+  uint32_t indexBytes;
+  void *pPages = NULL;
+  uint32_t streams = 0;
+  uint32_t streamBytes = 0;
+  int fUnused = 0;
+  sPdbRoot2 *ppr = NULL;
+
+  streamBytes = pph->rootStream.streamBytes;
+  pPages = pph->rootPages;
+
+  if ((ppr = pdb2FileRead (pph, streamBytes, pPages, &fUnused)) != NULL)
+    streams = ppr->nstreams;
+  if (ppr == NULL)
+    streamBytes = 0;
+  *pStreams = streams;
+  *pStreamBytes = streamBytes;
+  *pfUnused = fUnused;
+  return ppr;
+}
+
+static sPdbRoot7 *pdb7Root (sPdbHeader7 *pph, uint32_t *pStreams, uint32_t *pStreamBytes, int *pfUnused)
+{
+  uint32_t indexBytes;
+  void *pPages = NULL;
+  uint32_t streams = 0;
+  uint32_t streamBytes = 0;
+  int fUnused = 0;
+  sPdbRoot7 *ppr = NULL;
+
+  streamBytes = pph->rootBytes;
+  pPages = pph->indexPages;
+
+  indexBytes = pdb7FilePages (pph, streamBytes, NULL, NULL) * sizeof (uint32_t);
+
+  if ((pPages = pdb7FileRead (pph, indexBytes,  pPages, &fUnused)) != NULL)
+    {
+      if ((ppr = pdb7FileRead (pph, streamBytes, pPages, &fUnused)) != NULL)
+        streams = ppr->nstreams;
+      free (pPages);
+    }
+  if (ppr == NULL)
+  streamBytes = 0;
+  if (pStreams != NULL)
+    *pStreams = streams;
+  if (pStreamBytes != NULL)
+    *pStreamBytes = streamBytes;
+  if (pfUnused != NULL)
+    *pfUnused = fUnused;
+  return ppr;
+}
+
+static void *pdb2FileRead (sPdbHeader2 *pph, uint32_t streamBytes, void *pages, int *pfUnused)
+{
+  uint32_t pageBytes, dPages, i, j, n;
+  int fUnused = 0;
+  void *stream = NULL;
+
+  if (pages != NULL)
+    {
+      dPages = pdb2FilePages (pph, streamBytes, &pageBytes, &fUnused);
+      if (pageBytes)
+        stream = malloc (streamBytes);
+      if (stream != NULL)
+	{
+          n = streamBytes;
+	  for (i = 0; i < dPages; i++)
+	    {
+	      j = ((uint16_t *) pages)[i];
+	      memcpy ( &((unsigned char*)stream)[i * pageBytes], &((unsigned char *)pph)[j * pageBytes], (n < pageBytes ? n : pageBytes));
+	      n -= pageBytes;
+	    }
+	}
+    }
+  if (pfUnused != NULL)
+    *pfUnused = fUnused;
+  return stream;
+}
+
+static void *pdb7FileRead (sPdbHeader7 *pph, uint32_t streamBytes, void *pages, int *pfUnused)
+{
+  uint32_t pageBytes, dPages, i, j, n;
+  int fUnused = 0;
+  void *stream = NULL;
+
+  if (pages != NULL)
+    {
+      dPages = pdb7FilePages (pph, streamBytes, &pageBytes, &fUnused);
+      n = streamBytes;
+      if (pageBytes)
+        stream = malloc (n);
+      if (stream != NULL)
+	{
+	  for (i = 0; i < dPages; i++)
+	    {
+	      j = ((uint32_t *) pages)[i];
+	      memcpy (((unsigned char *) stream) + i * pageBytes, ((unsigned char *) pph) + j * pageBytes, (n < pageBytes ? n : pageBytes));
+	      n -= pageBytes;
+	    }
+	}
+    }
+  if (pfUnused != NULL)
+    *pfUnused = fUnused;
+  return stream;
+}
+
+static uint32_t pdb2FilePages (sPdbHeader2 *pph, uint32_t streamBytes, uint32_t *pPageBytes, int *pfUnused)
+{
+  uint32_t pageBytes = 0;
+  int fUnused = pdb2FileUnused (pph, streamBytes, &pageBytes);
+  *pPageBytes = pageBytes;
+  *pfUnused = fUnused;
+
+  return (pageBytes && streamBytes ? ((streamBytes - 1) / pageBytes) + 1 : 0);
+}
+
+static uint32_t pdb7FilePages (sPdbHeader7 *pph, uint32_t streamBytes, uint32_t *pPageBytes, int *pfUnused)
+{
+  uint32_t pageBytes = 0;
+  int fUnused = pdb7FileUnused (pph, streamBytes, &pageBytes);
+
+  if (pPageBytes != NULL)
+    *pPageBytes = pageBytes;
+  if (pfUnused != NULL)
+    *pfUnused = fUnused;
+
+  return (pageBytes && streamBytes ? ((streamBytes - 1) / pageBytes) + 1 : 0);
+}
+
+static int pdb2FileUnused (sPdbHeader2 *pph, uint32_t streamBytes, uint32_t *pPageBytes)
+{
+  if (streamBytes != PDB_UNUSED_16)
+    {
+      *pPageBytes = pph->pageBytes;
+      return 0;
+    }
+  *pPageBytes = 0;
+  return 1;
+}
+
+static int pdb7FileUnused (sPdbHeader7 *pph, uint32_t streamBytes, uint32_t *pPageBytes)
+{
+  if (streamBytes != PDB_UNUSED_32)
+    {
+      *pPageBytes = pph->pageBytes;
+      return 0;
+    }
+  *pPageBytes = 0;
+  return 1;
 }
--- experimental/tools/libmsdebug/dbg_pdb.h
+++ experimental/tools/libmsdebug/dbg_pdb.h
@@ -3,6 +3,16 @@
 
 #include <stdint.h>
 
+#pragma pack(push,1)
+
+/* Constants for unused streams.  */
+#define PDB_UNUSED_16       ((uint16_t) -1)
+#define PDB_UNUSED_32       ((uint32_t) -1)
+
+/* Type index base/limit.  */
+#define PDB_TI_MIN          0x00001000
+#define PDB_TI_MAX          0x00FFFFFF
+
 typedef struct sPdbStream2
 {
   uint32_t streamBytes;	/* stream size (-1 = unused).  */
@@ -31,5 +41,20 @@ typedef struct sPdbHeader7
   uint32_t reserved;		/* 0.  */
   uint32_t indexPages[];		/* root page index pages.  */
 } sPdbHeader7;
+
+typedef struct sPdbRoot2
+{
+  uint16_t nstreams;		/* number of streams.  */
+  uint16_t reserved;		/* not used.  */
+  sPdbStream2 streams[];	/* stream size list.  */
+} sPdbRoot2;
+
+typedef struct sPdbRoot7
+{
+  uint32_t nstreams;		/* number of streams.  */
+  uint32_t streamBytes[];	/* stream size list.  */
+} sPdbRoot7;
+
+#pragma pack(pop)
 
 #endif
