c6dac5ca26e83e4586a4f030ceeb79a2ba619ee2
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2009-06-03 18:57:54 +0000

    Add types for info stream (#1).
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@893 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/tools/libmsdebug/dbg_interface_pdb.h
+++ experimental/tools/libmsdebug/dbg_interface_pdb.h
@@ -2,6 +2,7 @@
 #define  _DBG_INTERFACE_PDB_HXX
 
 #include "dbg_interface.h"
+#include "dbg_pdb_info.h"
 
 extern sDbgInterface interface_pdb2; /* pdb 2.0 */
 extern sDbgInterface interface_pdb7; /* pdb 7.0 */
@@ -10,6 +11,7 @@ typedef struct sDbgInterfacePDB {
   sDbgInterface common;
   sDbgMemFile **files;
   uint32_t streams;
+  sPdbInfo *info; /* Stream #1  */
 } sDbgInterfacePDB;
 
 
