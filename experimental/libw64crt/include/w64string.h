e3d5af24d0346b29db7c540b574ff4d9d8c3d9f4
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2009-11-24 13:00:26 +0000

    Add strncpy.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@1539 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/libw64crt/include/w64string.h
+++ experimental/libw64crt/include/w64string.h
@@ -5,6 +5,7 @@
 
 size_t strlen (const char *);
 char *strcpy (char *, const char *);
+char *strncpy (char *, const char *, size_t);
 int strcmp (const char *, const char *);
 
 void *memcpy (void *, const void *, size_t);
