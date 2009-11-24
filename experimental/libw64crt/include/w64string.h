51476b46aac31a73ba25e4be6d4459deeacc3f9c
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2009-11-24 14:50:51 +0000

    Add strupr, strlwr, strnset, strset, memchr, memcmp, strcmpi, strnlen, memicmp, memset, memccpy
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@1543 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/libw64crt/include/w64string.h
+++ experimental/libw64crt/include/w64string.h
@@ -4,18 +4,29 @@
 #include <w64crt_defs.h>
 
 size_t strlen (const char *);
+size_t strnlen (const char *, size_t);
 char *strcpy (char *, const char *);
 char *strncpy (char *, const char *, size_t);
 int strcmp (const char *, const char *);
 int stricmp (const char *, const char *);
+int strcmpi (const char *, const char *);
 int strncmp (const char *, const char *, size_t);
 int strnicmp (const char *, const char *, size_t);
 char *strcat (char *, const char *);
 char *strncat (char *, const char *, size_t);
 char *strchr (const char *, int);
 char *strrchr (const char *, int);
+char *strnset (char *, int, size_t);
+char *strset (char *, int);
+char *strlwr (char *);
+char *strupr (char *);
 
 void *memcpy (void *, const void *, size_t);
+void *memccpy (void *, const void *, int, size_t);
+void *memset (void *, int, size_t);
+void *memchr (const void *, int, size_t);
+int memcmp (const void *, const void *, size_t);
+int memicmp (const void *s1, const void *s2, size_t n);
 
 #endif
 
