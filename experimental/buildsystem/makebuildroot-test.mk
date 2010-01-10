5795ed9df520a26cf93467c854b6adc6e62fd95d
jon_y <jon_y@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-01-10 01:30:11 +0000

    Arrange dependencies to avoid $< errors.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@1725 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildsystem/makebuildroot-test.mk
+++ experimental/buildsystem/makebuildroot-test.mk
@@ -201,9 +201,9 @@ gmp-extract: \
     src/gmp/.gmp.extract.marker
 
 src/gmp/.gmp.extract.marker: \
-    src/patches/.patches.pull.marker \
     src/gmp.tar.bz2 \
-    src/gmp/src/.mkdir.marker
+    src/gmp/src/.mkdir.marker \
+    src/patches/.patches.pull.marker
 	tar -C $(dir $@)/src --strip-components=1 -xjvf $<
 	@touch $@
 
@@ -239,8 +239,8 @@ mpfr-extract: \
 
 src/mpfr/.mpfr.extract.marker: \
     src/mpfr.tar.bz2 \
-    src/patches/.patches.pull.marker \
-    src/mpfr/src/.mkdir.marker
+    src/mpfr/src/.mkdir.marker \
+    src/patches/.patches.pull.marker
 	cd $(dir $@)src && patch -Np1 -i ../../patches/mpfr/cumulative-2.4.2-p1
 	cd $(dir $@)src && patch -Np1 -i ../../patches/mpfr/mpfr_vasprintf.patch
 	tar -C $(dir $@)/src --strip-components=1 -xjvf $<
