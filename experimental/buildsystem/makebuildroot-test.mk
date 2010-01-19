2a78463ba0fc2d216f181f7cab9a5e8d3498474c
jon_y <jon_y@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-01-19 14:19:33 +0000

    Update mpfr patch.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@1756 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildsystem/makebuildroot-test.mk
+++ experimental/buildsystem/makebuildroot-test.mk
@@ -253,7 +253,7 @@ src/mpfr/.mpfr.extract.marker: \
     src/mpfr/src/.mkdir.marker \
     src/patches/.patches.pull.marker
 	tar -C $(dir $@)/src --strip-components=1 -xjvf $<
-	cd $(dir $@)src && patch -Np1 -i ../../patches/mpfr/cumulative-2.4.2-p1
+	cd $(dir $@)src && patch -Np1 -i ../../patches/mpfr/cumulative-2.4.2-p3
 	@touch $@
 
 ########################################
