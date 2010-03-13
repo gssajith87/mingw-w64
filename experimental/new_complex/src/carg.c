a8a3861a9522f6f30c4c0bde0a6f3268afe134ee
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-03-13 17:04:32 +0000

    Change from assembly inline to real C-math function call.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2046 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/new_complex/src/carg.c
+++ experimental/new_complex/src/carg.c
@@ -1,38 +1,21 @@
 #include <math.h>
 #include <complex.h>
 
-/* carg?(z) = atan2?(__imag__ z, __real__ z);  */
-
 double
 carg (double complex z)
 {
-  double ret;
-
-  __asm__ ("fpatan;"
-           : "=t" (ret) : "0" (__real__ z), "u" (__imag__ z) : "st(1)");
-
-  return ret;
+  return atan2 (__imag__ z, __real__ z);
 }
 
 float
 cargf (float complex z)
 {
-  float ret;
-
-  __asm__ ("fpatan;"
-           : "=t" (ret) : "0" (__real__ z), "u" (__imag__ z) : "st(1)");
-
-  return ret;
+  return atan2f (__imag__ z, __real__ z);
 }
 
 long double
 cargl (long double complex z)
 {
-  long double ret;
-
-  __asm__ ("fpatan;"
-           : "=t" (ret) : "0" (__real__ z), "u" (__imag__ z) : "st(1)");
-
-  return ret;
+  return atan2l (__imag__ z, __real__ z);
 }
 
