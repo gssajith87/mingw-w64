15a16d1336309d3c802888252b96a1bfc5e9d04f
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-03-13 16:16:54 +0000

    First the easiest thing. cabs, cabsf, cabsl by hypot.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2038 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/new_complex/src/cabs.c
+++ experimental/new_complex/src/cabs.c
@@ -1,13 +1,19 @@
 #include <complex.h>
 
-double cabs(double complex z) {
-  return 0;
+double
+cabs (double complex z)
+{
+  return _hypot (__real__ z, __imag__ z);
 }
 
-float cabsf(float complex z) {
-  return 0;
+float
+cabsf (float complex z)
+{
+  return (float) _hypot (__real__ z, __imag__ z);
 }
 
-long double cabsl(long double complex z) {
-  return 0;
+long double
+cabsl (long double complex z)
+{
+  return hypotl (__real__ z, __imag__ z);
 }
