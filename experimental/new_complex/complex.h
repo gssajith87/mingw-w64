d423019a404580c85065fe2a7eff1e6c88f10153
NightStrike <NightStrike@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-03-13 15:31:39 +0000

    line wrapping
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2034 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/new_complex/complex.h
+++ experimental/new_complex/complex.h
@@ -56,8 +56,7 @@ float cabsf(float complex z);
 long double cabsl(long double complex z);
 double complex cpow(double complex x, double complex y);
 float complex cpowf(float complex x, float complex y);
-long double complex cpowl(long double complex x,
-long double complex y);
+long double complex cpowl(long double complex x, long double complex y);
 double complex csqrt(double complex z);
 float complex csqrtf(float complex z);
 long double complex csqrtl(long double complex z);
