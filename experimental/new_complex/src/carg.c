2b408973ec9541be4d5740810815c1a4e2d0d6a5
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-03-13 16:46:11 +0000

    Add next easy function series carg, cargf, and cargl.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2044 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/new_complex/src/carg.c
+++ experimental/new_complex/src/carg.c
@@ -1,13 +1,38 @@
+#include <math.h>
 #include <complex.h>
 
-double carg(double complex z) {
-  return 0;
+/* carg?(z) = atan2?(__imag__ z, __real__ z);  */
+
+double
+carg (double complex z)
+{
+  double ret;
+
+  __asm__ ("fpatan;"
+           : "=t" (ret) : "0" (__real__ z), "u" (__imag__ z) : "st(1)");
+
+  return ret;
 }
 
-float cargf(float complex z) {
-  return 0;
+float
+cargf (float complex z)
+{
+  float ret;
+
+  __asm__ ("fpatan;"
+           : "=t" (ret) : "0" (__real__ z), "u" (__imag__ z) : "st(1)");
+
+  return ret;
 }
 
-long double cargl(long double complex z) {
-  return 0;
+long double
+cargl (long double complex z)
+{
+  long double ret;
+
+  __asm__ ("fpatan;"
+           : "=t" (ret) : "0" (__real__ z), "u" (__imag__ z) : "st(1)");
+
+  return ret;
 }
+
