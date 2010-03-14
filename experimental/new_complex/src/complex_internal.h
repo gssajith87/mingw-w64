45f65047283635196059bd392cae014e87407fbf
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-03-14 10:46:58 +0000

    Add an library internal header to do standard includes and define some necessary constants.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2055 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ experimental/new_complex/src/complex_internal.h
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <complex.h>
+
+/* Define some PI constants for long double, as they are not defined in math.h  */
+#ifndef M_PI_4l
+#define M_PI_4l 0.7853981633974483096156608458198757L
+#define M_PI_2l 1.5707963267948966192313216916397514L
+#define M_PIl 3.1415926535897932384626433832795029L
+#endif
+
+/* NAN builtins for gcc, as they are not part of math.h  */
+#ifndef NANF
+#define NANF __builtin_nanf ("")
+#endif
+#ifndef NANL
+#define NANL __builtin_nanl ("")
+#endif
+
+/* Some more helpers.  */
+#define M_PI_3_4 (M_PI - M_PI_4)
+#define M_PI_3_4l (M_PIl - M_PI_4l)
+
