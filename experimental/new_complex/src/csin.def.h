cbe1eaac1be36c7752b524c12f19f316add255f9
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-03-15 12:11:01 +0000

    Cleanup logic in ccosh, csinh, and csin functions.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2059 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/new_complex/src/csin.def.h
+++ experimental/new_complex/src/csin.def.h
@@ -9,23 +9,13 @@ __FLT_ABI(csin) (__FLT_TYPE __complex__ z)
 
   __real__ z = __FLT_ABI(fabs) (__real__ z);
 
-  if (i_class != FP_NAN && i_class != FP_INFINITE)
+  if (i_class == FP_NAN)
   {
-    if (r_class != FP_NAN && r_class != FP_INFINITE)
-    {
-      __FLT_ABI(sincos) (__real__ z, &s_x, &c_x);
+    __real__ ret = (r_class == FP_ZERO ?
+      (__FLT_ABI(copysign) (__FLT_CST(0.0), negate ? -__FLT_CST(1.0) : __FLT_CST(1.0)))
+      : __FLT_NAN);
+    __imag__ ret = __FLT_NAN;
 
-      __real__ ret = __FLT_ABI(cosh) (__imag__ z) * s_x;
-      __imag__ ret = __FLT_ABI(sinh) (__imag__ z) * c_x;
-
-      if (negate)
-	__real__ ret = -__real__ ret;
-    }
-    else
-    {
-      __real__ ret = __FLT_NAN;
-      __imag__ ret = (i_class == FP_ZERO ? __imag__ z : __FLT_NAN);
-    }
     return ret;
   }
 
@@ -35,31 +25,43 @@ __FLT_ABI(csin) (__FLT_TYPE __complex__ z)
     {
       __real__ ret = __FLT_ABI(copysign) (__FLT_CST(0.0), negate ? -__FLT_CST(1.0) : __FLT_CST(1.0));
       __imag__ ret = __imag__ z;
+      return ret;
     }
-    else if (r_class > FP_ZERO)
-    {
-      __FLT_ABI(sincos) (__real__ z, &s_x, &c_x);
 
-      __real__ ret = __FLT_ABI(copysign) (__FLT_HUGE_VAL, s_x);
-      __imag__ ret = __FLT_ABI(copysign) (__FLT_HUGE_VAL, c_x);
-
-      if (negate)
-	__real__ ret = -__real__ ret;
-      if (signbit (__imag__ z))
-	__imag__ ret = -__imag__ ret;
-    }
-    else
+    if (r_class == FP_NAN || r_class == FP_INFINITE)
     {
       __real__ ret = __FLT_NAN;
       __imag__ ret = __FLT_HUGE_VAL;
+      return ret;
     }
+
+    __FLT_ABI(sincos) (__real__ z, &s_x, &c_x);
+
+    __real__ ret = __FLT_ABI(copysign) (__FLT_HUGE_VAL, s_x);
+    __imag__ ret = __FLT_ABI(copysign) (__FLT_HUGE_VAL, c_x);
+
+    if (negate)
+      __real__ ret = -__real__ ret;
+    if (signbit (__imag__ z))
+      __imag__ ret = -__imag__ ret;
+
     return ret;
   }
 
-  __real__ ret = (r_class == FP_ZERO ?
-    (__FLT_ABI(copysign) (__FLT_CST(0.0), negate ? -__FLT_CST(1.0) : __FLT_CST(1.0)))
-    : __FLT_NAN);
-  __imag__ ret = __FLT_NAN;
+  if (r_class == FP_NAN || r_class == FP_INFINITE)
+  {
+    __real__ ret = __FLT_NAN;
+    __imag__ ret = (i_class == FP_ZERO ? __imag__ z : __FLT_NAN);
+    return ret;
+  }
+
+  __FLT_ABI(sincos) (__real__ z, &s_x, &c_x);
+
+  __real__ ret = __FLT_ABI(cosh) (__imag__ z) * s_x;
+  __imag__ ret = __FLT_ABI(sinh) (__imag__ z) * c_x;
+
+  if (negate)
+    __real__ ret = -__real__ ret;
 
   return ret;
 }
--- experimental/new_complex/src/csinh.def.h
+++ experimental/new_complex/src/csinh.def.h
@@ -9,31 +9,11 @@ __FLT_ABI(csinh) (__FLT_TYPE __complex__ z)
 
   __real__ z = __FLT_ABI(fabs) (__real__ z);
 
-  if (r_class != FP_NAN && r_class != FP_INFINITE)
+  if (r_class == FP_NAN)
   {
-    if (i_class != FP_NAN && i_class != FP_INFINITE)
-    {
-      __FLT_ABI(sincos) (__imag__ z, &s_x, &c_x);
+    __real__ ret = __FLT_NAN;
+    __imag__ ret = __imag__ z == __FLT_CST(0.0) ? __imag__ z : __FLT_NAN;
 
-      __real__ ret = __FLT_ABI(sinh) (__real__ z) * c_x;
-      __imag__ ret = __FLT_ABI(cosh) (__real__ z) * s_x;
-
-      if (negate)
-	__real__ ret = -__real__ ret;
-    }
-    else
-    {
-      if (r_class == FP_ZERO)
-      {
-	__real__ ret = __FLT_ABI(copysign) (__FLT_CST(0.0), negate ? -__FLT_CST(1.0) : __FLT_CST(1.0));
-	__imag__ ret = __FLT_NAN + __FLT_NAN;
-      }
-      else
-      {
-	__real__ ret = __FLT_NAN;
-	__imag__ ret = __FLT_NAN;
-      }
-    }
     return ret;
   }
 
@@ -43,27 +23,41 @@ __FLT_ABI(csinh) (__FLT_TYPE __complex__ z)
     {
       __real__ ret = negate ? -__FLT_HUGE_VAL : __FLT_HUGE_VAL;
       __imag__ ret = __imag__ z;
+      return ret;
     }
-    else if (i_class > FP_ZERO)
-    {
-      __FLT_ABI(sincos) (__imag__ z, &s_x, &c_x);
 
-      __real__ ret = __FLT_ABI(copysign) (__FLT_HUGE_VAL, c_x);
-      __imag__ ret = __FLT_ABI(copysign) (__FLT_HUGE_VAL, s_x);
-
-      if (negate)
-	__real__ ret = -__real__ ret;
-    }
-    else
+    if (i_class == FP_NAN || i_class == FP_INFINITE)
     {
       __real__ ret = __FLT_HUGE_VAL;
       __imag__ ret = __FLT_NAN + __FLT_NAN;
+
+      return ret;
     }
+    __FLT_ABI(sincos) (__imag__ z, &s_x, &c_x);
+
+    __real__ ret = __FLT_ABI(copysign) (__FLT_HUGE_VAL, c_x);
+    __imag__ ret = __FLT_ABI(copysign) (__FLT_HUGE_VAL, s_x);
+
+    if (negate)
+    __real__ ret = -__real__ ret;
     return ret;
   }
 
-  __real__ ret = __FLT_NAN;
-  __imag__ ret = __imag__ z == __FLT_CST(0.0) ? __imag__ z : __FLT_NAN;
+  if (i_class == FP_NAN || i_class == FP_INFINITE)
+  {
+    __real__ ret = (r_class == FP_ZERO
+      ? (__FLT_ABI(copysign) (__FLT_CST(0.0), negate ? -__FLT_CST(1.0) : __FLT_CST(1.0))) : __FLT_NAN);
+    __imag__ ret = (r_class == FP_ZERO ? (__FLT_NAN + __FLT_NAN) : __FLT_NAN);
 
+    return ret;
+  }
+
+  __FLT_ABI(sincos) (__imag__ z, &s_x, &c_x);
+
+  __real__ ret = __FLT_ABI(sinh) (__real__ z) * c_x;
+  __imag__ ret = __FLT_ABI(cosh) (__real__ z) * s_x;
+
+  if (negate)
+    __real__ ret = -__real__ ret;
   return ret;
 }
