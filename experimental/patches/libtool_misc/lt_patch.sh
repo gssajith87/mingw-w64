8c2b7e860befbab8e6e2c71ef2e2487547060265
sezero <sezero@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-03-31 18:30:42 +0000

    added patches/libtool_misc/lt_patch.sh, a small shell script for patching the libtoolized files of a source package for pe-x86_64 magic.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2130 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ experimental/patches/libtool_misc/lt_patch.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+# When run in the root directory of a source package, this script
+# patches all the libtoolized files for pe-x86_64 magic.  one may
+# try re-libtoolizing with the latest development version of libtool,
+# but doing so may give unexpected results, especially if the project
+# in question is using a customized libtool version.  This script
+# modifies only one necessary line in a given file, instead.
+#
+# Requires sed and touch for the modifications, diff and diffstat for
+# final report.
+
+libtool_patch()
+{
+ echo "Applying patch for libtool pe-x86_64 magic.."
+ rm -f tmp.0lt.diff && touch tmp.0lt.diff
+ lst="libtool.m4 aclocal.m4 ltmain.sh libtool configure"
+ for i in $lst
+ do
+  j=`find . -type f -name $i`
+  for f in $j
+  do
+   if test -f $f; then
+    cp -p $f tmp.0lt
+    sed -e "s|EGREP[-e ]*'file format pe-i386(\.\*architecture: i386)?' >|EGREP 'file format (pei\*-i386(\.\*architecture:\ i386)?\|pe-arm-wince\|pe-x86-64)' >|" \
+	-e "s|EGREP[-e ]*'file format (pe-i386(\.\*architecture: i386)?\|pe-arm-wince\|pe-x86-64)' >|EGREP 'file format (pei\*-i386(\.\*architecture:\ i386)?\|pe-arm-wince\|pe-x86-64)' >|" \
+	-e "s|deplibs_check_method='file_magic file format pei\*-i386(\.\*architecture: i386)?'|deplibs_check_method='file_magic file format (pei\*-i386(\.\*architecture:\ i386)?\|pe-arm-wince\|pe-x86-64)'|" \
+	-e "s|deplibs_check_method='file_magic file format pe-i386(\.\*architecture: i386)?'|deplibs_check_method='file_magic file format (pei\*-i386(\.\*architecture:\ i386)?\|pe-arm-wince\|pe-x86-64)'|" \
+      tmp.0lt > $f
+    diff -up tmp.0lt $f >> tmp.0lt.diff
+#   preserve the original file timestamp to avoid autotools' reconfiguration
+    touch -r tmp.0lt $f
+    rm -f tmp.0lt
+   fi
+  done
+ done
+# tell us who actually changed:
+ diffstat -l -p0 tmp.0lt.diff 2> /dev/null
+}
+
+libtool_patch
+
