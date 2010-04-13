gssajith87/mingw-w64

commit 4a44e129eb94f2722b35d7bc77763cf8c0becd5d

Add a bunch of missing sublangs found in w32api 3.12
git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2178 4407c894-4637-0410-b4f5-ada5f102cad1

trunk/mingw-w64-headers/include/winnt.h
@@ -675,13 +675,16 @@ typedef DWORD LCID;
 #define LANG_YORUBA 0x6a
 #define LANG_ZULU 0x35
 
-#define SUBLANG_NEUTRAL 0x0
-#define SUBLANG_DEFAULT 0x1
-#define SUBLANG_SYS_DEFAULT 0x2
-#define SUBLANG_CUSTOM_DEFAULT 0x3
-#define SUBLANG_CUSTOM_UNSPECIFIED 0x4
-#define SUBLANG_UI_CUSTOM_DEFAULT 0x5
-
+#define SUBLANG_NEUTRAL 0x00
+#define SUBLANG_DEFAULT 0x01
+#define SUBLANG_SYS_DEFAULT 0x02
+#define SUBLANG_CUSTOM_DEFAULT 0x03
+#define SUBLANG_CUSTOM_UNSPECIFIED 0x04
+#define SUBLANG_UI_CUSTOM_DEFAULT 0x03
+#define SUBLANG_AFRIKAANS_SOUTH_AFRICA 0x01
+#define SUBLANG_ALBANIAN_ALBANIA 0x01
+#define SUBLANG_ALSATIAN_FRANCE 0x01
+#define SUBLANG_AMHARIC_ETHIOPIA 0x01
 #define SUBLANG_ARABIC_SAUDI_ARABIA 0x01
 #define SUBLANG_ARABIC_IRAQ 0x02
 #define SUBLANG_ARABIC_EGYPT 0x03
@@ -698,13 +701,30 @@ typedef DWORD LCID;
 #define SUBLANG_ARABIC_UAE 0x0e
 #define SUBLANG_ARABIC_BAHRAIN 0x0f
 #define SUBLANG_ARABIC_QATAR 0x10
+#define SUBLANG_ARMENIAN_ARMENIA 0x01
+#define SUBLANG_ASSAMESE_INDIA 0x01
 #define SUBLANG_AZERI_LATIN 0x01
 #define SUBLANG_AZERI_CYRILLIC 0x02
+#define SUBLANG_BASHKIR_RUSSIA 0x01
+#define SUBLANG_BASQUE_BASQUE 0x01
+#define SUBLANG_BELARUSIAN_BELARUS 0x01
+#define SUBLANG_BENGALI_INDIA 0x01
+#define SUBLANG_BOSNIAN_BOSNIA_HERZEGOVINA_LATIN 0x05
+#define SUBLANG_BOSNIAN_BOSNIA_HERZEGOVINA_CYRILLIC 0x08
+#define SUBLANG_BRETON_FRANCE 0x01
+#define SUBLANG_BULGARIAN_BULGARIA 0x01
+#define SUBLANG_CATALAN_CATALAN 0x01
 #define SUBLANG_CHINESE_TRADITIONAL 0x01
 #define SUBLANG_CHINESE_SIMPLIFIED 0x02
 #define SUBLANG_CHINESE_HONGKONG 0x03
 #define SUBLANG_CHINESE_SINGAPORE 0x04
 #define SUBLANG_CHINESE_MACAU 0x05
+#define SUBLANG_CORSICAN_FRANCE 0x01
+#define SUBLANG_CZECH_CZECH_REPUBLIC 0x01
+#define SUBLANG_CROATIAN_CROATIA 0x01
+#define SUBLANG_CROATIAN_BOSNIA_HERZEGOVINA_LATIN 0x04
+#define SUBLANG_DANISH_DENMARK 0x01
+#define SUBLANG_DIVEHI_MALDIVES 0x01
 #define SUBLANG_DUTCH 0x01
 #define SUBLANG_DUTCH_BELGIAN 0x02
 #define SUBLANG_ENGLISH_US 0x01
@@ -712,6 +732,9 @@ typedef DWORD LCID;
 #define SUBLANG_ENGLISH_AUS 0x03
 #define SUBLANG_ENGLISH_CAN 0x04
 #define SUBLANG_ENGLISH_NZ 0x05
+#if (WINVER >= 0x0600)
+#define SUBLANG_ENGLISH_IRELAND 0x06
+#endif
 #define SUBLANG_ENGLISH_EIRE 0x06
 #define SUBLANG_ENGLISH_SOUTH_AFRICA 0x07
 #define SUBLANG_ENGLISH_JAMAICA 0x08
@@ -720,32 +743,111 @@ typedef DWORD LCID;
 #define SUBLANG_ENGLISH_TRINIDAD 0x0b
 #define SUBLANG_ENGLISH_ZIMBABWE 0x0c
 #define SUBLANG_ENGLISH_PHILIPPINES 0x0d
+#define SUBLANG_ENGLISH_INDIA 0x10
+#define SUBLANG_ENGLISH_MALAYSIA 0x11
+#define SUBLANG_ENGLISH_SINGAPORE 0x12
+#define SUBLANG_ESTONIAN_ESTONIA 0x01
+#define SUBLANG_FAEROESE_FAROE_ISLANDS 0x01
+#define SUBLANG_FILIPINO_PHILIPPINES 0x01
+#define SUBLANG_FINNISH_FINLAND 0x01
 #define SUBLANG_FRENCH 0x01
 #define SUBLANG_FRENCH_BELGIAN 0x02
 #define SUBLANG_FRENCH_CANADIAN 0x03
 #define SUBLANG_FRENCH_SWISS 0x04
 #define SUBLANG_FRENCH_LUXEMBOURG 0x05
 #define SUBLANG_FRENCH_MONACO 0x06
+#define SUBLANG_FRISIAN_NETHERLANDS 0x01
+#define SUBLANG_GALICIAN_GALICIAN 0x01
+#define SUBLANG_GEORGIAN_GEORGIA 0x01
 #define SUBLANG_GERMAN 0x01
 #define SUBLANG_GERMAN_SWISS 0x02
 #define SUBLANG_GERMAN_AUSTRIAN 0x03
 #define SUBLANG_GERMAN_LUXEMBOURG 0x04
 #define SUBLANG_GERMAN_LIECHTENSTEIN 0x05
+#define SUBLANG_GREEK_GREECE 0x01
+#define SUBLANG_GREENLANDIC_GREENLAND 0x01
+#define SUBLANG_GUJARATI_INDIA 0x01
+#define SUBLANG_HAUSA_NIGERIA 0x01
+#define SUBLANG_HEBREW_ISRAEL 0x01
+#define SUBLANG_HINDI_INDIA 0x01
+#define SUBLANG_HUNGARIAN_HUNGARY 0x01
+#define SUBLANG_ICELANDIC_ICELAND 0x01
+#define SUBLANG_IGBO_NIGERIA 0x01
+#define SUBLANG_INDONESIAN_INDONESIA 0x01
+#define SUBLANG_INUKTITUT_CANADA 0x01
+#define SUBLANG_INUKTITUT_CANADA_LATIN 0x01
+#define SUBLANG_IRISH_IRELAND 0x01
 #define SUBLANG_ITALIAN 0x01
 #define SUBLANG_ITALIAN_SWISS 0x02
-#define SUBLANG_KASHMIRI_SASIA 0x02
+#define SUBLANG_JAPANESE_JAPAN 0x01
 #define SUBLANG_KASHMIRI_INDIA 0x02
+#define SUBLANG_KASHMIRI_SASIA 0x02
+#define SUBLANG_KAZAK_KAZAKHSTAN 0x01
+#define SUBLANG_KHMER_CAMBODIA 0x01
+#define SUBLANG_KICHE_GUATEMALA 0x01
+#define SUBLANG_KINYARWANDA_RWANDA 0x01
+#define SUBLANG_KONKANI_INDIA 0x01
 #define SUBLANG_KOREAN 0x01
+#define SUBLANG_KYRGYZ_KYRGYZSTAN 0x01
+#define SUBLANG_LAO_LAO_PDR 0x01
+#define SUBLANG_LATVIAN_LATVIA 0x01
+#if (WINVER >= 0x0600)
+#define SUBLANG_LITHUANIAN_LITHUANIA 0x01
+#endif
 #define SUBLANG_LITHUANIAN 0x01
+#define SUBLANG_LOWER_SORBIAN_GERMANY 0x01
+#define SUBLANG_LUXEMBOURGISH_LUXEMBOURG 0x01
+#define SUBLANG_MACEDONIAN_MACEDONIA 0x01
 #define SUBLANG_MALAY_MALAYSIA 0x01
 #define SUBLANG_MALAY_BRUNEI_DARUSSALAM 0x02
+#define SUBLANG_MALAYALAM_INDIA 0x01
+#define SUBLANG_MALTESE_MALTA 0x01
+#define SUBLANG_MAORI_NEW_ZEALAND 0x01
+#define SUBLANG_MAPUDUNGUN_CHILE 0x01
+#define SUBLANG_MARATHI_INDIA 0x01
+#define SUBLANG_MOHAWK_MOHAWK 0x01
+#define SUBLANG_MONGOLIAN_CYRILLIC_MONGOLIA 0x01
+#define SUBLANG_MONGOLIAN_PRC 0x02
+#define SUBLANG_NEPALI_NEPAL 0x01
 #define SUBLANG_NEPALI_INDIA 0x02
 #define SUBLANG_NORWEGIAN_BOKMAL 0x01
 #define SUBLANG_NORWEGIAN_NYNORSK 0x02
-#define SUBLANG_PORTUGUESE 0x02
+#define SUBLANG_OCCITAN_FRANCE 0x01
+#define SUBLANG_ORIYA_INDIA 0x01
+#define SUBLANG_PASHTO_AFGHANISTAN 0x01
+#define SUBLANG_PERSIAN_IRAN 0x01
+#define SUBLANG_POLISH_POLAND 0x01
 #define SUBLANG_PORTUGUESE_BRAZILIAN 0x01
+#if (WINVER >= 0x0600)
+#define SUBLANG_PORTUGUESE_PORTUGAL 0x02
+#endif
+#define SUBLANG_PORTUGUESE 0x02
+#define SUBLANG_PUNJABI_INDIA 0x01
+#define SUBLANG_QUECHUA_BOLIVIA 0x01
+#define SUBLANG_QUECHUA_ECUADOR 0x02
+#define SUBLANG_QUECHUA_PERU 0x03
+#define SUBLANG_ROMANIAN_ROMANIA 0x01
+#define SUBLANG_ROMANSH_SWITZERLAND 0x01
+#define SUBLANG_RUSSIAN_RUSSIA 0x01
+#define SUBLANG_SAMI_NORTHERN_NORWAY 0x01
+#define SUBLANG_SAMI_NORTHERN_SWEDEN 0x02
+#define SUBLANG_SAMI_NORTHERN_FINLAND 0x03
+#define SUBLANG_SAMI_LULE_NORWAY 0x04
+#define SUBLANG_SAMI_LULE_SWEDEN 0x05
+#define SUBLANG_SAMI_SOUTHERN_NORWAY 0x06
+#define SUBLANG_SAMI_SOUTHERN_SWEDEN 0x07
+#define SUBLANG_SAMI_SKOLT_FINLAND 0x03
+#define SUBLANG_SAMI_INARI_FINLAND 0x03
+#define SUBLANG_SANSKRIT_INDIA 0x01
 #define SUBLANG_SERBIAN_LATIN 0x02
 #define SUBLANG_SERBIAN_CYRILLIC 0x03
+#define SUBLANG_SERBIAN_BOSNIA_HERZEGOVINA_LATIN 0x06
+#define SUBLANG_SERBIAN_BOSNIA_HERZEGOVINA_CYRILLIC 0x07
+#define SUBLANG_SINDHI_AFGHANISTAN 0x02
+#define SUBLANG_SINHALESE_SRI_LANKA 0x01
+#define SUBLANG_SOTHO_NORTHERN_SOUTH_AFRICA 0x01
+#define SUBLANG_SLOVAK_SLOVAKIA 0x01
+#define SUBLANG_SLOVENIAN_SLOVENIA 0x01
 #define SUBLANG_SPANISH 0x01
 #define SUBLANG_SPANISH_MEXICAN 0x02
 #define SUBLANG_SPANISH_MODERN 0x03
@@ -766,8 +868,17 @@ typedef DWORD LCID;
 #define SUBLANG_SPANISH_HONDURAS 0x12
 #define SUBLANG_SPANISH_NICARAGUA 0x13
 #define SUBLANG_SPANISH_PUERTO_RICO 0x14
+#define SUBLANG_SPANISH_US 0x15
+#if (WINVER >= 0x0600)
+#define SUBLANG_SWEDISH_SWEDEN 0x01
+#endif
 #define SUBLANG_SWEDISH 0x01
 #define SUBLANG_SWEDISH_FINLAND 0x02
+#define SUBLANG_SYRIAC 0x01
+#define SUBLANG_TAJIK_TAJIKISTAN 0x01
+#define SUBLANG_TAMAZIGHT_ALGERIA_LATIN 0x02
+#define SUBLANG_TAMIL_INDIA 0x01
+#define SUBLANG_TATAR_RUSSIA 0x01
 #define SUBLANG_TELUGU_INDIA 0x01
 #define SUBLANG_THAI_THAILAND 0x01
 #define SUBLANG_TIBETAN_PRC 0x01
@@ -775,10 +886,22 @@ typedef DWORD LCID;
 #define SUBLANG_TIGRIGNA_ERITREA 0x01
 #define SUBLANG_TSWANA_SOUTH_AFRICA 0x01
 #define SUBLANG_TURKISH_TURKEY 0x01
+#define SUBLANG_TURKMEN_TURKMENISTAN 0x01
+#define SUBLANG_UIGHUR_PRC 0x01
+#define SUBLANG_UKRAINIAN_UKRAINE 0x01
+#define SUBLANG_UPPER_SORBIAN_GERMANY 0x01
 #define SUBLANG_URDU_PAKISTAN 0x01
 #define SUBLANG_URDU_INDIA 0x02
 #define SUBLANG_UZBEK_LATIN 0x01
 #define SUBLANG_UZBEK_CYRILLIC 0x02
+#define SUBLANG_VIETNAMESE_VIETNAM 0x01
+#define SUBLANG_WELSH_UNITED_KINGDOM 0x01
+#define SUBLANG_WOLOF_SENEGAL 0x01
+#define SUBLANG_YORUBA_NIGERIA 0x01
+#define SUBLANG_XHOSA_SOUTH_AFRICA 0x01
+#define SUBLANG_YAKUT_RUSSIA 0x01
+#define SUBLANG_YI_PRC 0x01
+#define SUBLANG_ZULU_SOUTH_AFRICA 0x01
 
 #define SORT_DEFAULT 0x0
 #define SORT_INVARIANT_MATH 0x1

commit 0297db41fe1d6a530f2df8f6155cfbe0c9e51775

2010-04-13  Ozkan Sezer  <sezeroz@gmail.com>
	* winnt.h: Fix several SUBLANG ID errors introduced by r2178 and 2179
	(ref: http://msdn.microsoft.com/en-us/library/dd318693(VS.85).aspx).
	(SUBLANG_UI_CUSTOM_DEFAULT): 0x05, not 0x03.
	(SUBLANG_HAUSA_NIGERIA): Bad ID name, correct one is ...
	(SUBLANG_HAUSA_NIGERIA_LATIN): ... this.
	(SUBLANG_INUKTITUT_CANADA_LATIN): 0x02, not 0x01.
	(SUBLANG_IRISH_IRELAND): 0x02, not 0x01.
	(SUBLANG_LAO_LAO_PDR): Bad ID name, correct one is ...
	(SUBLANG_LAO_LAO): ... this.
	(SUBLANG_LOWER_SORBIAN_GERMANY): 0x02, not 0x01.
	(SUBLANG_SAMI_SKOLT_FINLAND): 0x08, not 0x03.
	(SUBLANG_SAMI_INARI_FINLAND): 0x09, not 0x03.
	(SUBLANG_SYRIAC_SYRIA): define as SUBLANG_SYRIAC (what MSDN mentions).
	(SUBLANG_TIGRIGNA_ERITREA): 0x02, not 0x01.


git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2180 4407c894-4637-0410-b4f5-ada5f102cad1

trunk/mingw-w64-headers/include/winnt.h
@@ -680,7 +680,8 @@ typedef DWORD LCID;
 #define SUBLANG_SYS_DEFAULT 0x02
 #define SUBLANG_CUSTOM_DEFAULT 0x03
 #define SUBLANG_CUSTOM_UNSPECIFIED 0x04
-#define SUBLANG_UI_CUSTOM_DEFAULT 0x03
+#define SUBLANG_UI_CUSTOM_DEFAULT 0x05
+
 #define SUBLANG_AFRIKAANS_SOUTH_AFRICA 0x01
 #define SUBLANG_ALBANIAN_ALBANIA 0x01
 #define SUBLANG_ALSATIAN_FRANCE 0x01
@@ -767,7 +768,8 @@ typedef DWORD LCID;
 #define SUBLANG_GREEK_GREECE 0x01
 #define SUBLANG_GREENLANDIC_GREENLAND 0x01
 #define SUBLANG_GUJARATI_INDIA 0x01
-#define SUBLANG_HAUSA_NIGERIA 0x01
+#define SUBLANG_HAUSA_NIGERIA_LATIN 0x01
+#define SUBLANG_HAUSA_NIGERIA SUBLANG_HAUSA_NIGERIA_LATIN /* SUBLANG_HAUSA_NIGERIA_LATIN is what MS defines */
 #define SUBLANG_HEBREW_ISRAEL 0x01
 #define SUBLANG_HINDI_INDIA 0x01
 #define SUBLANG_HUNGARIAN_HUNGARY 0x01
@@ -775,8 +777,8 @@ typedef DWORD LCID;
 #define SUBLANG_IGBO_NIGERIA 0x01
 #define SUBLANG_INDONESIAN_INDONESIA 0x01
 #define SUBLANG_INUKTITUT_CANADA 0x01
-#define SUBLANG_INUKTITUT_CANADA_LATIN 0x01
-#define SUBLANG_IRISH_IRELAND 0x01
+#define SUBLANG_INUKTITUT_CANADA_LATIN 0x02
+#define SUBLANG_IRISH_IRELAND 0x02
 #define SUBLANG_ITALIAN 0x01
 #define SUBLANG_ITALIAN_SWISS 0x02
 #define SUBLANG_JAPANESE_JAPAN 0x01
@@ -789,13 +791,14 @@ typedef DWORD LCID;
 #define SUBLANG_KONKANI_INDIA 0x01
 #define SUBLANG_KOREAN 0x01
 #define SUBLANG_KYRGYZ_KYRGYZSTAN 0x01
-#define SUBLANG_LAO_LAO_PDR 0x01
+#define SUBLANG_LAO_LAO 0x01
+#define SUBLANG_LAO_LAO_PDR SUBLANG_LAO_LAO /* SUBLANG_LAO_LAO is what MS defines */
 #define SUBLANG_LATVIAN_LATVIA 0x01
 #if (WINVER >= 0x0600)
 #define SUBLANG_LITHUANIAN_LITHUANIA 0x01
 #endif
 #define SUBLANG_LITHUANIAN 0x01
-#define SUBLANG_LOWER_SORBIAN_GERMANY 0x01
+#define SUBLANG_LOWER_SORBIAN_GERMANY 0x02
 #define SUBLANG_LUXEMBOURGISH_LUXEMBOURG 0x01
 #define SUBLANG_MACEDONIAN_MACEDONIA 0x01
 #define SUBLANG_MALAY_MALAYSIA 0x01
@@ -836,8 +839,8 @@ typedef DWORD LCID;
 #define SUBLANG_SAMI_LULE_SWEDEN 0x05
 #define SUBLANG_SAMI_SOUTHERN_NORWAY 0x06
 #define SUBLANG_SAMI_SOUTHERN_SWEDEN 0x07
-#define SUBLANG_SAMI_SKOLT_FINLAND 0x03
-#define SUBLANG_SAMI_INARI_FINLAND 0x03
+#define SUBLANG_SAMI_SKOLT_FINLAND 0x08
+#define SUBLANG_SAMI_INARI_FINLAND 0x09
 #define SUBLANG_SANSKRIT_INDIA 0x01
 #define SUBLANG_SERBIAN_LATIN 0x02
 #define SUBLANG_SERBIAN_CYRILLIC 0x03
@@ -875,6 +878,7 @@ typedef DWORD LCID;
 #define SUBLANG_SWEDISH 0x01
 #define SUBLANG_SWEDISH_FINLAND 0x02
 #define SUBLANG_SYRIAC 0x01
+#define SUBLANG_SYRIAC_SYRIA SUBLANG_SYRIAC /* SUBLANG_SYRIAC_SYRIA is what MSDN mentions */
 #define SUBLANG_TAJIK_TAJIKISTAN 0x01
 #define SUBLANG_TAMAZIGHT_ALGERIA_LATIN 0x02
 #define SUBLANG_TAMIL_INDIA 0x01
@@ -883,7 +887,7 @@ typedef DWORD LCID;
 #define SUBLANG_THAI_THAILAND 0x01
 #define SUBLANG_TIBETAN_PRC 0x01
 #define SUBLANG_TIBETAN_BHUTAN 0x02
-#define SUBLANG_TIGRIGNA_ERITREA 0x01
+#define SUBLANG_TIGRIGNA_ERITREA 0x02
 #define SUBLANG_TSWANA_SOUTH_AFRICA 0x01
 #define SUBLANG_TURKISH_TURKEY 0x01
 #define SUBLANG_TURKMEN_TURKMENISTAN 0x01