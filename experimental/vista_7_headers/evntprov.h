479b18b735398fd6547595716b8ecc19dcfcf7b1
jon_y <jon_y@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-07-01 13:50:02 +0000

    Preliminary merge of evntrace.h, ktmtypes.h and evntprov.h from ROS
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2736 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/vista_7_headers/evntprov.h
+++ experimental/vista_7_headers/evntprov.h
@@ -1,6 +1,27 @@
 #ifndef _INC_EVNTPROV
 #define _INC_EVNTPROV
 #if (_WIN32_WINNT >= 0x0600)
+
+#ifndef EVNTAPI
+#ifndef MIDL_PASS
+#ifdef _EVNT_SOURCE_
+#define EVNTAPI __stdcall
+#else
+#define EVNTAPI DECLSPEC_IMPORT __stdcall
+#endif /* _EVNT_SOURCE_ */
+#endif /* MIDL_PASS */
+#endif /* EVNTAPI */
+
+#ifndef FORCEINLINE
+#ifdef __GNUC__
+#define FORCEINLINE __attribute((always_inline))
+#endif /*__GNUC__*/
+#endif /*FORCEINLINE*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <windows.h>
 #include <guiddef.h>
 
@@ -8,8 +29,18 @@
 #define EVENT_CONTROL_CODE_ENABLE_PROVIDER 1
 #define EVENT_CONTROL_CODE_CAPTURE_STATE 2
 
-typedef LPVOID PREGHANDLE;
-typedef LPVOID PENABLECALLBACK;
+typedef ULONGLONG REGHANDLE, *PREGHANDLE;
+typedef struct _EVENT_FILTER_DESCRIPTOR *PEVENT_FILTER_DESCRIPTOR;
+
+typedef VOID
+(NTAPI *PENABLECALLBACK)(
+  LPCGUID SourceId,
+  ULONG IsEnabled,
+  UCHAR Level,
+  ULONGLONG MatchAnyKeyword,
+  ULONGLONG MatchAllKeyword,
+  PEVENT_FILTER_DESCRIPTOR FilterData,
+  PVOID CallbackContext);
 
 typedef struct _EVENT_DESCRIPTOR {
   USHORT    Id;
@@ -44,7 +75,7 @@ typedef struct _EVENT_FILTER_HEADER {
   ULONG     NextOffset;
 } EVENT_FILTER_HEADER, *PEVENT_FILTER_HEADER;
 
-ULONG EventWriteEx(
+ULONG EVNTAPI EventWriteEx(
   REGHANDLE RegHandle,
   PCEVENT_DESCRIPTOR EventDescriptor,
   ULONG64 Filter,
@@ -122,6 +153,189 @@ ULONG NTAPI EventWriteString(
   ULONGLONG Keyword,
   PCWSTR String
 );
+
+
+FORCEINLINE
+VOID
+EventDataDescCreate(
+  OUT PEVENT_DATA_DESCRIPTOR EventDataDescriptor,
+  IN const VOID* DataPtr,
+  IN ULONG DataSize)
+{
+  EventDataDescriptor->Ptr = (ULONGLONG)(ULONG_PTR)DataPtr;
+  EventDataDescriptor->Size = DataSize;
+  EventDataDescriptor->Reserved = 0;
+}
+
+FORCEINLINE
+VOID
+EventDescCreate(
+  OUT PEVENT_DESCRIPTOR EventDescriptor,
+  IN USHORT Id,
+  IN UCHAR Version,
+  IN UCHAR Channel,
+  IN UCHAR Level,
+  IN USHORT Task,
+  IN UCHAR Opcode,
+  IN ULONGLONG Keyword)
+{
+  EventDescriptor->Id = Id;
+  EventDescriptor->Version = Version;
+  EventDescriptor->Channel = Channel;
+  EventDescriptor->Level = Level;
+  EventDescriptor->Task = Task;
+  EventDescriptor->Opcode = Opcode;
+  EventDescriptor->Keyword = Keyword;
+}
+
+FORCEINLINE
+VOID
+EventDescZero(
+  OUT PEVENT_DESCRIPTOR EventDescriptor)
+{
+  memset(EventDescriptor, 0, sizeof(EVENT_DESCRIPTOR));
+}
+
+FORCEINLINE
+USHORT
+EventDescGetId(
+  IN PCEVENT_DESCRIPTOR EventDescriptor)
+{
+  return (EventDescriptor->Id);
+}
+
+FORCEINLINE
+UCHAR
+EventDescGetVersion(
+  IN PCEVENT_DESCRIPTOR EventDescriptor)
+{
+  return (EventDescriptor->Version);
+}
+
+FORCEINLINE
+USHORT
+EventDescGetTask(
+  IN PCEVENT_DESCRIPTOR EventDescriptor)
+{
+  return (EventDescriptor->Task);
+}
+
+FORCEINLINE
+UCHAR
+EventDescGetOpcode(
+  IN PCEVENT_DESCRIPTOR EventDescriptor)
+{
+  return (EventDescriptor->Opcode);
+}
+
+FORCEINLINE
+UCHAR
+EventDescGetChannel(
+  IN PCEVENT_DESCRIPTOR EventDescriptor)
+{
+  return (EventDescriptor->Channel);
+}
+
+FORCEINLINE
+UCHAR
+EventDescGetLevel(
+  IN PCEVENT_DESCRIPTOR EventDescriptor)
+{
+  return (EventDescriptor->Level);
+}
+
+FORCEINLINE
+ULONGLONG
+EventDescGetKeyword(
+  IN PCEVENT_DESCRIPTOR EventDescriptor)
+{
+  return (EventDescriptor->Keyword);
+}
+
+FORCEINLINE
+PEVENT_DESCRIPTOR
+EventDescSetId(
+  IN PEVENT_DESCRIPTOR EventDescriptor,
+  IN USHORT Id)
+{
+  EventDescriptor->Id = Id;
+  return (EventDescriptor);
+}
+
+FORCEINLINE
+PEVENT_DESCRIPTOR
+EventDescSetVersion(
+  IN PEVENT_DESCRIPTOR EventDescriptor,
+  IN UCHAR Version)
+{
+  EventDescriptor->Version = Version;
+  return (EventDescriptor);
+}
+
+FORCEINLINE
+PEVENT_DESCRIPTOR
+EventDescSetTask(
+  IN PEVENT_DESCRIPTOR EventDescriptor,
+  IN USHORT Task)
+{
+  EventDescriptor->Task = Task;
+  return (EventDescriptor);
+}
+
+FORCEINLINE
+PEVENT_DESCRIPTOR
+EventDescSetOpcode(
+  IN PEVENT_DESCRIPTOR EventDescriptor,
+  IN UCHAR Opcode)
+{
+  EventDescriptor->Opcode = Opcode;
+  return (EventDescriptor);
+}
+
+FORCEINLINE
+PEVENT_DESCRIPTOR
+EventDescSetLevel(
+  IN PEVENT_DESCRIPTOR EventDescriptor,
+  IN UCHAR  Level)
+{
+  EventDescriptor->Level = Level;
+  return (EventDescriptor);
+}
+
+FORCEINLINE
+PEVENT_DESCRIPTOR
+EventDescSetChannel(
+  IN PEVENT_DESCRIPTOR EventDescriptor,
+  IN UCHAR Channel)
+{
+  EventDescriptor->Channel = Channel;
+  return (EventDescriptor);
+}
+
+FORCEINLINE
+PEVENT_DESCRIPTOR
+EventDescSetKeyword(
+  IN PEVENT_DESCRIPTOR EventDescriptor,
+  IN ULONGLONG Keyword)
+{
+  EventDescriptor->Keyword = Keyword;
+  return (EventDescriptor);
+}
+
+
+FORCEINLINE
+PEVENT_DESCRIPTOR
+EventDescOrKeyword(
+  IN PEVENT_DESCRIPTOR EventDescriptor,
+  IN ULONGLONG Keyword)
+{
+  EventDescriptor->Keyword |= Keyword;
+  return (EventDescriptor);
+}
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /*(_WIN32_WINNT >= 0x0600)*/
 #endif /*_INC_EVNTPROV*/
