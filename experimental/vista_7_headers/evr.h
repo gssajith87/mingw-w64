77260243443fb835fc9d701b9f52f404edd77c21
sezero <sezero@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-07-17 20:00:26 +0000

    evr.h: Fix misplaced extern "C" {
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2871 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/vista_7_headers/evr.h
+++ experimental/vista_7_headers/evr.h
@@ -6,18 +6,17 @@
 #ifndef _INC_EVR
 #define _INC_EVR
 
+#if (_WIN32_WINNT >= 0x0600)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if (_WIN32_WINNT >= 0x0600)
 #if (_WIN32_WINNT >= 0x0601)
-
 typedef enum _EVRFilterConfig_Prefs {
   EVRFilterConfigPrefs_EnableQoS   = 0x00000001,
-  EVRFilterConfigPrefs_Mask        = 0x00000001 
+  EVRFilterConfigPrefs_Mask        = 0x00000001
 } EVRFilterConfigPrefs;
-
 #endif /*(_WIN32_WINNT >= 0x0601)*/
 
 typedef enum MFVideoAspectRatioMode {
@@ -25,19 +24,17 @@ typedef enum MFVideoAspectRatioMode {
   MFVideoARMode_PreservePicture    = 0x00000001,
   MFVideoARMode_PreservePixel      = 0x00000002,
   MFVideoARMode_NonLinearStretch   = 0x00000004,
-  MFVideoARMode_Mask               = 0x00000007 
+  MFVideoARMode_Mask               = 0x00000007
 } MFVideoAspectRatioMode;
 
 #if (_WIN32_WINNT >= 0x0601)
-
 typedef enum _MFVideoMixPrefs {
   MFVideoMixPrefs_ForceHalfInterlace         = 0x00000001,
   MFVideoMixPrefs_AllowDropToHalfInterlace   = 0x00000002,
   MFVideoMixPrefs_AllowDropToBob             = 0x00000004,
   MFVideoMixPrefs_ForceBob                   = 0x00000008,
-  MFVideoMixPrefs_Mask                       = 0x0000000f 
+  MFVideoMixPrefs_Mask                       = 0x0000000f
 } MFVideoMixPrefs;
-
 #endif /*(_WIN32_WINNT >= 0x0601)*/
 
 typedef enum MFVideoRenderPrefs {
@@ -50,7 +47,7 @@ typedef enum MFVideoRenderPrefs {
   MFVideoRenderPrefs_ForceScaling            = 0x00000040,
   MFVideoRenderPrefs_AllowScaling            = 0x00000080,
   MFVideoRenderPrefs_DoNotRepaintOnStop      = 0x00000100,
-  MFVideoRenderPrefs_Mask                    = 0x000000ff 
+  MFVideoRenderPrefs_Mask                    = 0x000000ff
 } MFVideoRenderPrefs;
 
 typedef enum _MFVP_MESSAGE_TYPE {
@@ -61,7 +58,7 @@ typedef enum _MFVP_MESSAGE_TYPE {
   MFVP_MESSAGE_ENDSTREAMING          = 0x00000004,
   MFVP_MESSAGE_ENDOFSTREAM           = 0x00000005,
   MFVP_MESSAGE_STEP                  = 0x00000006,
-  MFVP_MESSAGE_CANCELSTEP            = 0x00000007 
+  MFVP_MESSAGE_CANCELSTEP            = 0x00000007
 } MFVP_MESSAGE_TYPE;
 
 typedef struct MFVideoNormalizedRect {
@@ -99,6 +96,7 @@ DECLARE_INTERFACE_(IMFVideoPresenter,IMFClockStateSink)
 
     END_INTERFACE
 };
+
 #ifdef COBJMACROS
 #define IMFVideoPresenter_QueryInterface(This,riid,ppvObject) (This)->pVtbl->QueryInterface(This,riid,ppvObject)
 #define IMFVideoPresenter_AddRef(This) (This)->pVtbl->AddRef(This)
@@ -155,6 +153,7 @@ DECLARE_INTERFACE_(IMFTrackedSample,IUnknown)
 
     END_INTERFACE
 };
+
 #ifdef COBJMACROS
 #define IMFTrackedSample_QueryInterface(This,riid,ppvObject) (This)->pVtbl->QueryInterface(This,riid,ppvObject)
 #define IMFTrackedSample_AddRef(This) (This)->pVtbl->AddRef(This)
@@ -175,6 +174,7 @@ HRESULT WINAPI MFCreateVideoSampleFromSurface(IUnknown *pUnkSurface,IMFSample **
 #ifdef __cplusplus
 }
 #endif
+
 #endif /*(_WIN32_WINNT >= 0x0600)*/
 
 #endif /*_INC_EVR*/
