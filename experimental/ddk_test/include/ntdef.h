5efccb399ca747985ba5c7736dbbd1ebe882364d
sezero <sezero@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-07-24 09:00:20 +0000

    * ntdef.h: undefine UNALIGNED and UNALIGNED64 to avoid redefinition warnings against _mingw.h.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@2956 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/ddk_test/include/ntdef.h
+++ experimental/ddk_test/include/ntdef.h
@@ -176,6 +176,10 @@
 
 #endif /* __ANONYMOUS_DEFINED */
 
+/* undefine UNALIGNED and UNALIGNED64 to avoid
+ * redefinition warnings against _mingw.h.  */
+#undef  UNALIGNED
+#undef  UNALIGNED64
 #if defined(_M_MRX000) || defined(_M_ALPHA) || defined(_M_PPC) || defined(_M_IA64) || defined(_M_AMD64)
 #define ALIGNMENT_MACHINE
 #define UNALIGNED __unaligned
