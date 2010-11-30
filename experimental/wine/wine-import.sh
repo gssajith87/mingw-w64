ce809c35b0bab3dc3bf6cade4fce6430f2652e94
cjacek <cjacek@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-11-30 11:20:10 +0000

    wine-import.sh: Updated imported header list.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@3829 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/wine/wine-import.sh
+++ experimental/wine/wine-import.sh
@@ -36,7 +36,7 @@ done
 
 # DirectX headers
 for f in d3d10effect.h d3d10misc.h d3d10shader.h d3d8.h d3d8caps.h d3d8types.h d3d9.h d3d9caps.h d3d9types.h d3dhal.h \
-	d3dtypes.h d3dx9core.h d3dx9tex.h dxerr8.h dxerr9.h errors.h xinput.h; do
+	d3dtypes.h d3dx9core.h d3dx9tex.h dplay.h dsdriver.h dxerr8.h dxerr9.h errors.h xinput.h; do
     import_header $f direct-x/include
 done
 
