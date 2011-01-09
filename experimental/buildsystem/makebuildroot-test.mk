0f452843fed8b879234c858a0b33a3e4d6e394b4
jon_y <jon_y@4407c894-4637-0410-b4f5-ada5f102cad1>
2011-01-09 01:57:52 +0000

    Make sure --build gets passed to headers configure.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@3944 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildsystem/makebuildroot-test.mk
+++ experimental/buildsystem/makebuildroot-test.mk
@@ -472,7 +472,8 @@ ${BUILD_DIR}/mingw-headers/obj/.config.marker: \
 	  ${CURDIR}/${BUILD_DIR}/mingw/mingw-w64-headers/configure \
 	  --prefix=${CURDIR}/${BUILD_DIR}/root \
 	  --with-sysroot=${CURDIR}/${BUILD_DIR}/root \
-	  --host=${TARGET_ARCH}
+	  --host=${TARGET_ARCH} \
+	  ${CONFIG_BUILD_ARGS}
 	@touch $@
 
 ########################################
