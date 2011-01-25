ae3eaa0c10f05a6f921d3ed0f4778ee707844d83
jon_y <jon_y@4407c894-4637-0410-b4f5-ada5f102cad1>
2011-01-25 09:38:13 +0000

    Fix copy and paste typo.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@3989 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/dfp_math/lroundd128.c
+++ experimental/dfp_math/lroundd128.c
@@ -1,3 +1,3 @@
 #define _RET_TYPE long int
-#define _DFP32_FLOAT
+#define _DFP128_FLOAT
 #include "lroundd.def"
--- experimental/dfp_math/lroundd64.c
+++ experimental/dfp_math/lroundd64.c
@@ -1,3 +1,3 @@
 #define _RET_TYPE long int
-#define _DFP128_FLOAT
+#define _DFP64_FLOAT
 #include "lroundd.def"
