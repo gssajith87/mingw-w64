8306a4b85ffd6fb3c79b0c27f822fa25252446eb
nightstrike <nightstrike@4407c894-4637-0410-b4f5-ada5f102cad1>
2011-08-21 16:28:03 +0000

    Remove shellcommand conditional from upload factory, and add step to remove the file after it has been uploaded
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64@4404 4407c894-4637-0410-b4f5-ada5f102cad1

--- experimental/buildbot/factories/sfuploadfactory.py
+++ experimental/buildbot/factories/sfuploadfactory.py
@@ -9,7 +9,6 @@ from buildbot.process import factory
 from buildbot.steps.source import CVS, SVN
 from buildbot.steps.shell import Configure, Compile, ShellCommand, WithProperties, SetProperty
 from buildbot.steps.transfer import FileDownload, FileUpload
-from scripts.buildsteps import ShellCommandConditional
 
 class SourceForgeUploadFactory(factory.BuildFactory):
 
@@ -21,20 +20,26 @@ class SourceForgeUploadFactory(factory.BuildFactory):
     # masterdir - the path to the master configuration (has tarballs + scripts + passwords)
     # filename  - the name of the file to upload (relative to masterdir)
     # destname  - how to name the file on the server
-    self.addStep(ShellCommandConditional,
-                 condprop="is_nightly",
-                 name="sfupload",
-                 description=["uploading"],
-                 descriptionDone=["upload"],
-                 workdir=WithProperties("%(masterdir)s"),
-                 command=["scripts/upload.py",
-                          WithProperties("%(filename)s"),
-                          WithProperties("%(destname)s"),
-                          WithProperties("%(target-os:+--os)s"),
-                          WithProperties("%(target-os:-)s"),
-                          WithProperties("%(path:+--path)s"),
-                          WithProperties("%(path:-)s"),
-                          WithProperties("%(datestamp:+--datestamp)s"),
-                          WithProperties("%(datestamp:-)s")],
-                 haltOnFailure=True)
+    self.addStep(ShellCommand(name="sfupload",
+                              description=["uploading"],
+                              descriptionDone=["upload"],
+                              doStepIf=lambda step: (step.build.getProperties().has_key("is_nightly") and step.build.getProperty("is_nightly") ),
+                              workdir=WithProperties("%(masterdir)s"),
+                              command=["scripts/upload.py",
+                                       WithProperties("%(filename)s"),
+                                       WithProperties("%(destname)s"),
+                                       WithProperties("%(target-os:+--os)s"),
+                                       WithProperties("%(target-os:-)s"),
+                                       WithProperties("%(path:+--path)s"),
+                                       WithProperties("%(path:-)s"),
+                                       WithProperties("%(datestamp:+--datestamp)s"),
+                                       WithProperties("%(datestamp:-)s")],
+                              haltOnFailure=True))
+
+    self.addStep(ShellCommand(name="uploadComplete",
+                              description=["removing", "temporary", "file"],
+                              descriptionDone=["temporary", "file", "removed"],
+                              doStepIf=lambda step: (step.build.getProperties().has_key("is_nightly") and step.build.getProperty("is_nightly") ),
+                              workdir=WithProperties("%(masterdir)s"),
+                              command=["rm", "-f", WithProperties("%(filename)s")]))
 
